0f0978b478a62ce906cb971e429b398851cc6a1a
Tuan Pham Anh <baabeetaa@gmail.com>
2023-09-29 20:19:55 +0700

    add evince service (#2)
    https://github.com/ingenuity-build/evince

--- evince/docker_service_create.sh
+++ evince/docker_service_create.sh
@@ -5,8 +5,11 @@ docker service rm evince
 docker service create \
   --name evince \
   --replicas 1 \
+  --publish mode=host,target=1323,published=1323 \
   --network net1 \
+  --constraint 'node.hostname==nmisc1' \
   --restart-condition none \
+  --endpoint-mode dnsrr \
   archlinux:latest \
   /bin/bash -c \
-  "curl -s https://raw.githubusercontent.com/notional-labs/nmisc/1-add-evince-service/evince/run.sh > ~/run.sh && /bin/bash ~/run.sh"
+  "curl -s https://raw.githubusercontent.com/notional-labs/nmisc/main/evince/run.sh > ~/run.sh && /bin/bash ~/run.sh"
