a66288ed9fc45e9dc980529f4d12dbd112d80097
Tuan Pham Anh <baabeetaa@gmail.com>
2023-10-08 11:00:29 +0700

    add proxy

---
+++ proxy/run.sh
@@ -0,0 +1,20 @@
+pacman -Syu --noconfirm
+pacman -S --noconfirm base-devel wget dnsutils nginx cronie screen
+
+########################################################################################################################
+# SSL (fullchain.pem and privkey.pem files)
+wget "http://tasks.web_config/config/notional.ventures.fullchain.pem" -O /etc/nginx/fullchain.pem
+wget "http://tasks.web_config/config/notional.ventures.privkey.pem" -O /etc/nginx/privkey.pem
+
+########################################################################################################################
+# nginx
+curl -s "https://raw.githubusercontent.com/notional-labs/nmisc/main/proxy/nginx.conf" > $HOME/nginx.conf.template
+
+# run nginx with screen to avoid log to docker
+screen -S nginx -dm /usr/sbin/nginx -g "daemon off;"
+
+########################################################################################################################
+echo "Done!"
+
+# loop forever for debugging only
+while true; do sleep 60; done
