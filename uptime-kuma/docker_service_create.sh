62031d7fca0bd3ee6d872615ee7c73402f925731
Tuan Pham Anh <baabeetaa@gmail.com>
2023-10-08 10:00:48 +0700

    add uptime-kuma https://github.com/notional-labs/nmisc/issues/3

---
+++ uptime-kuma/docker_service_create.sh
@@ -0,0 +1,9 @@
+docker service create \
+  --name uptime-kuma \
+  --replicas 1 \
+  --publish target=3001,published=3001 \
+  --mount type=bind,source=/mnt/data/uptime-kuma,destination=/app/data \
+  --network net1 \
+  --constraint 'node.hostname==nmisc1' \
+  --restart-condition any \
+  louislam/uptime-kuma:1
