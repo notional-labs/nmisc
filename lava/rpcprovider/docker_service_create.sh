67c7bfe5fd24bf7d766ee78c9203a722abf4878f
Tuan Pham Anh <baabeetaa@gmail.com>
2023-10-08 17:52:31 +0700

    lava...

--- lava/rpcprovider/docker_service_create.sh
+++ lava/rpcprovider/docker_service_create.sh
@@ -1,13 +1,25 @@
+# usage: ./docker_service_create.sh chain_name
+# eg., ./docker_service_create.sh osmosis-testnet
+
+chain_name="$1"
+
+if [[ -z $chain_name ]]; then
+  echo "No chain_name. usage eg., ./docker_service_create.sh cosmoshub"
+  exit
+fi
+
+service_name="lava_rpcprovider_${chain_name}"
+
 # delete existing service
-docker service rm lava_rpcprovider
+docker service rm $service_name
 
 # create new service
 docker service create \
-  --name lava_rpcprovider \
+  --name $service_name \
   --replicas 1 \
   --network net1 \
   --sysctl 'net.ipv4.tcp_tw_reuse=1' \
   --restart-condition none \
   archlinux:latest \
   /bin/bash -c \
-  "curl -s https://raw.githubusercontent.com/notional-labs/nmisc/main/lava/rpcprovider/run.sh > ~/run.sh && /bin/bash ~/run.sh"
+  "curl -s https://raw.githubusercontent.com/notional-labs/nmisc/main/lava/rpcprovider/run.sh > ~/run.sh && /bin/bash ~/run.sh $chain_name"
--- lava/rpcprovider/run.sh
+++ lava/rpcprovider/run.sh
@@ -1,3 +1,13 @@
+# usage: ./run.sh chain_name
+# eg., ./run.sh osmosis-testnet
+
+chain_name="$1"
+if [[ -z $chain_name ]]; then
+  echo "No chain_name. usage eg., ./run.sh cosmoshub"
+  exit
+fi
+
+
 pacman -Syu --noconfirm
 pacman -S --noconfirm go git base-devel wget pigz jq cronie screen unzip logrotate
 
@@ -11,8 +21,8 @@ make install
 
 $HOME/go/bin/lavad config chain-id lava-testnet-2
 
-# create osmosis-testnet-provider.yml
-wget "http://tasks.web_config/config/lava.osmosis-testnet-provider.yml" -O $HOME/.lava/osmosis-testnet-provider.yml
+# create ${chain_name}-provider.yml
+wget "http://tasks.web_config/config/lava.${chain_name}-provider.yml" -O $HOME/.lava/${chain_name}-provider.yml
 
 # keyring-file
 wget -O - "http://tasks.web_config/config/lava.keyring-file.tar.gz" |tar -xzf - -C $HOME/.lava/
@@ -20,7 +30,7 @@ wget -O - "http://tasks.web_config/config/lava.keyring-file.tar.gz" |tar -xzf - 
 # start_rpcprovider.sh script
 cat <<EOT > $HOME/start_rpcprovider.sh
 cd $HOME/.lava
-echo "notional" | /root/go/bin/lavad rpcprovider osmosis-testnet-provider.yml --from notional --geolocation 2 --chain-id lava-testnet-2 --log_level debug --node https://public-rpc-testnet2.lavanet.xyz:443/rpc/
+echo "notional" | /root/go/bin/lavad rpcprovider ${chain_name}-provider.yml --from notional --geolocation 2 --chain-id lava-testnet-2 --log_level debug --node https://public-rpc-testnet2.lavanet.xyz:443/rpc/
 EOT
 
 # run
