39a0716c289265202017c6fa4934b4bb0b5ccadf
Tuan Pham Anh <baabeetaa@gmail.com>
2023-12-22 03:06:44 +0700

    ibc_monitor: ...

--- ibc_monitor/run.sh
+++ ibc_monitor/run.sh
@@ -13,9 +13,9 @@ pip install -r requirements.txt --break-system-packages
 
 wget "http://tasks.web_config/config/NOTIONAL_API_KEY_RELAYING" -O $HOME/NOTIONAL_API_KEY_RELAYING.txt
 NOTIONAL_API_KEY=$(cat $HOME/NOTIONAL_API_KEY_RELAYING.txt)
-#export NOTIONAL_API_KEY="$NOTIONAL_API_KEY"
+export NOTIONAL_API_KEY="$NOTIONAL_API_KEY"
 
-screen -S api -dm NOTIONAL_API_KEY=${NOTIONAL_API_KEY} /usr/sbin/python app.py
+screen -S api -dm /usr/sbin/python app.py
 
 ########################################################################################################################
 echo "Done!"
