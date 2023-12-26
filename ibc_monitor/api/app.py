51741c21a9510a7c3688873c31080a71b0f16197
Tuan Pham Anh <baabeetaa@gmail.com>
2023-12-26 18:37:13 +0700

    ibc_monitor: fix

--- ibc_monitor/api/app.py
+++ ibc_monitor/api/app.py
@@ -1,6 +1,6 @@
 import json
 import os
-
+import traceback
 import datetime
 import flask
 import requests
@@ -51,6 +51,8 @@ def get_ibc_status():
     hermes_config_url = flask.request.args.get('hermes_config_url')
     print("hermes_config_url: " + hermes_config_url)
 
+    res = []
+
     try:
         # url = "https://raw.githubusercontent.com/notional-labs/cosmosia/main/relaying/coreum_config.toml"
         rpc_request = requests.get(hermes_config_url)
@@ -58,54 +60,54 @@ def get_ibc_status():
         parsed_toml = toml.loads(body)
         chains = parsed_toml["chains"]
 
-        res = []
-
         for chain in chains:
-            chain_id = chain["id"]
-            chain_obj = map_chainid_to_name.get(chain_id)
+            try:
+                chain_id = chain["id"]
+                chain_obj = map_chainid_to_name.get(chain_id)
 
-            if chain_obj is not None:
-                channels = chain["packet_filter"]["list"]
-                for channel in channels:
-                    res_channel = {"chain_id": chain_id, "channel_id": channel[1], "client_id": "", "latest_height": "",
-                                   "counter_chain_id": "", "block_time": "", "time_ago": "", "pending_packets": -1}
+                if chain_obj is not None:
+                    channels = chain["packet_filter"]["list"]
+                    for channel in channels:
+                        res_channel = {"chain_id": chain_id, "channel_id": channel[1], "client_id": "", "latest_height": "",
+                                       "counter_chain_id": "", "block_time": "", "time_ago": "", "pending_packets": -1}
 
-                    base_url = "https://a-{}--{}.gw.notionalapi.com".format(chain_obj, NOTIONAL_API_KEY)
-                    if type(chain_obj) == dict:
-                        base_url = chain_obj["api"]
+                        base_url = "https://a-{}--{}.gw.notionalapi.com".format(chain_obj, NOTIONAL_API_KEY)
+                        if type(chain_obj) == dict:
+                            base_url = chain_obj["api"]
 
-                    try:
-                        url = "{}/ibc/core/channel/v1/channels/{}/ports/transfer/client_state".format(base_url, res_channel["channel_id"])
-                        rpc_request = requests.get(url)
-                        rpc_request_json = rpc_request.json()
+                        try:
+                            url = "{}/ibc/core/channel/v1/channels/{}/ports/transfer/client_state".format(base_url, res_channel["channel_id"])
+                            rpc_request = requests.get(url)
+                            rpc_request_json = rpc_request.json()
 
-                        res_channel["client_id"] = rpc_request_json["identified_client_state"]["client_id"]
-                        res_channel["latest_height"] = rpc_request_json["identified_client_state"]["client_state"]["latest_height"]["revision_height"]
-                        res_channel["counter_chain_id"] = rpc_request_json["identified_client_state"]["client_state"]["chain_id"]
+                            res_channel["client_id"] = rpc_request_json["identified_client_state"]["client_id"]
+                            res_channel["latest_height"] = rpc_request_json["identified_client_state"]["client_state"]["latest_height"]["revision_height"]
+                            res_channel["counter_chain_id"] = rpc_request_json["identified_client_state"]["client_state"]["chain_id"]
 
-                        counter_chain_obj = map_chainid_to_name.get(res_channel["counter_chain_id"] )
-                        res_channel["block_time"] = get_block_time(counter_chain_obj, res_channel["latest_height"])
-                        res_channel["time_ago"] = timeago.format(parseDate(res_channel["block_time"]), datetime.datetime.utcnow())
-                    except Exception:
-                        pass
+                            counter_chain_obj = map_chainid_to_name.get(res_channel["counter_chain_id"] )
+                            res_channel["block_time"] = get_block_time(counter_chain_obj, res_channel["latest_height"])
+                            res_channel["time_ago"] = timeago.format(parseDate(res_channel["block_time"]), datetime.datetime.utcnow())
+                        except Exception:
+                            pass
 
-                    try:
-                        url = "{}/ibc/core/channel/v1/channels/{}/ports/transfer/packet_commitments".format(base_url, res_channel["channel_id"])
-                        rpc_request = requests.get(url)
-                        rpc_request_json = rpc_request.json()
-                        pending_packets = int(rpc_request_json["pagination"]["total"])
-                        res_channel["pending_packets"] = pending_packets
-                    except Exception:
-                        pass
+                        try:
+                            url = "{}/ibc/core/channel/v1/channels/{}/ports/transfer/packet_commitments".format(base_url, res_channel["channel_id"])
+                            rpc_request = requests.get(url)
+                            rpc_request_json = rpc_request.json()
+                            pending_packets = int(rpc_request_json["pagination"]["total"])
+                            res_channel["pending_packets"] = pending_packets
+                        except Exception:
+                            pass
 
-                    print(res_channel)
-                    res.append(res_channel)
+                        print(res_channel)
+                        res.append(res_channel)
+            except Exception:
+                pass
+    except Exception:
+        # printing stack trace
+        traceback.print_exc()
 
-        return res, 200
-    except (Exception,):
-        print("Err")
-
-    return "error", 500
+    return res, 200
 
 
 if __name__ == '__main__':
