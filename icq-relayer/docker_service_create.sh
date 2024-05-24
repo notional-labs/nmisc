6cab22b66a22fb668b47d367b2c4dc0536df0bd4
Joe Bowman <joe@ingenuity.build>
2024-05-24 12:04:34 +0100

    add second instance of icq using different seed secret

--- icq-relayer/docker_service_create.sh
+++ icq-relayer/docker_service_create.sh
@@ -1,14 +1,27 @@
-# delete existing service
-docker service rm icq
+VERSION=v1.0.0-alpha.0
 
-# create new service
+# delete existing service
+docker service rm icq_1
+docker service rm icq_2
+
 docker service create \
-  --name icq \
+  --name icq_1 \
   --replicas 1 \
   --network net1 \
   --sysctl 'net.ipv4.tcp_tw_reuse=1' \
   --restart-condition any \
   --secret source=icq_prod_seed,target=seed \
   --entrypoint /usr/local/bin/entrypoint.sh \
-  quicksilverzone/interchain-queries:v1.0.0-alpha.0 \
-  icq-relayer start
+  quicksilverzone/interchain-queries:$VERSION \
+  icq-relayer start --home /icq/.icq-relayer
+
+docker service create \
+  --name icq_2 \
+  --replicas 1 \
+  --network net1 \
+  --sysctl 'net.ipv4.tcp_tw_reuse=1' \
+  --restart-condition any \
+  --secret source=icq_prod_seed2,target=seed \
+  --entrypoint /usr/local/bin/entrypoint.sh \
+  quicksilverzone/interchain-queries:$VERSION \
+  icq-relayer start --home /icq/.icq-relayer
